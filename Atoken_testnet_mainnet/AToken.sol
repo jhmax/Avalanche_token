834e578fb6f10b21cdbfb1668f0af5659e116d49
joao maximino <jhmaximino1@gmail.com>
2022-12-29 15:21:24 +0000

    initial commit

---
+++ Atoken_testnet_mainnet/AToken.sol
@@ -0,0 +1,32 @@
+
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract AToken is ERC20, Pausable, Ownable {
+    constructor() ERC20("AToken", "ATK") {
+        _mint(msg.sender, 2000000 * 10 ** decimals());
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
